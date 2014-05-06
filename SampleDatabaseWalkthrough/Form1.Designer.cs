606e897cd9518b400a0a7d2b7d8311235593b16f
szogun1987 <szogun1987@gmail.com>
2014-05-06 19:26:52 +0200

    Naprawienie builda

--- SampleDatabaseWalkthrough/Form1.Designer.cs
+++ SampleDatabaseWalkthrough/Form1.Designer.cs
@@ -148,7 +148,6 @@
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(186, 20);
             this.textBox2.TabIndex = 0;
-            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
             // 
             // radioButton4
             // 
