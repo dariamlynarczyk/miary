86d81c35987401682027e5fee617dcc73a499525
szogun1987 <szogun1987@gmail.com>
2014-10-05 12:28:13 +0200

    Dodanie resty DTO

---
+++ SampleDatabaseWalkthrough/SimpleDTO/NoteDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SampleDatabaseWalkthrough.SimpleDTO
+{
+    public class NoteDTO
+    {
+        public int Id { get; set; }
+
+        public string Content { get; set; }
+    }
+}
